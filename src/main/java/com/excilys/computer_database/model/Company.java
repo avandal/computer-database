01ec6131a6ccd53b716e0b6f7969561ee1455812
Alexander <avan-dalen@excilys.com>
2019-03-21 10:51:42 +0100

    surcharge hashcode/equals de Computer de Company

--- src/main/java/com/excilys/computer_database/model/Company.java
+++ src/main/java/com/excilys/computer_database/model/Company.java
@@ -10,22 +10,37 @@ public class Company {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null) return false;
-		if (this.getClass() != obj.getClass()) return false;
+		if (this == obj) 
+			return true;
+		if (obj == null) 
+			return false;
+		if (getClass() != obj.getClass()) 
+			return false;
 		
-		Company company = (Company) obj;
+		Company other = (Company) obj;
 		
-		if (this.id != company.id) return false;
-		
-		if (this.name == null && company.name != null) return false;
-		if (this.name != null && company.name == null) return false;
-		if (this.name != null && company.name != null && !this.name.equals(company.name)) return false;
+		if (id != other.id) 
+			return false;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name))
+			return false;
 		
 		return true;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
--- src/main/java/com/excilys/computer_database/model/Computer.java
+++ src/main/java/com/excilys/computer_database/model/Computer.java
@@ -15,38 +15,67 @@ public class Computer {
 		this.introduced = introduced;
 		this.discontinued = discontinued;
 		this.company = company;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null) return false;
-		if (this.getClass() != obj.getClass()) return false;
-		
-		Computer computer = (Computer) obj;
-		if (this.id != computer.id) return false;
-		
-		if (this.name == null && computer.name != null) return false;
-		if (this.name != null && computer.name == null) return false;
-		if (this.name != null && computer.name != null && !this.name.equals(computer.name)) return false;
-		
-		if (this.introduced == null && computer.introduced != null) return false;
-		if (this.introduced != null && computer.introduced == null) return false;
-		if (this.introduced != null && computer.introduced != null && !this.introduced.equals(computer.introduced)) return false;
-		
-		if (this.discontinued == null && computer.discontinued != null) return false;
-		if (this.discontinued != null && computer.discontinued == null) return false;
-		if (this.discontinued != null && computer.discontinued != null && !this.discontinued.equals(computer.discontinued)) return false;
-		
-		if (this.company == null && computer.company != null) return false;
-		if (this.company != null && computer.company == null) return false;
-		if (this.company != null && computer.company != null && !this.company.equals(computer.company)) return false;
-		
-		return true;
-	}
+	}	
 	
 	public int getId() {
 		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Computer other = (Computer) obj;
+		
+		if (id != other.id)
+			return false;
+		
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name))
+			return false;
+		
+		if (introduced == null) {
+			if (other.introduced != null) {
+				return false;
+			}
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		
+		if (discontinued == null) {
+			if (other.discontinued != null) {
+				return false;
+			}
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		
+		if (company == null) {
+			if (other.company != null) {
+				return false;
+			}
+		} else if (!company.equals(other.company))
+			return false;
+		
+		return true;
 	}
 
 	public void setId(int id) {
